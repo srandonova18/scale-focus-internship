dbe0c26d16b57eceb252efc357043053505e8367
srandonova18 <srandonova18@codingburgas.bg>
2021-07-15 00:00:51 +0300

    Define TASK structure.

---
+++ application/taskDefine.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+#include <nanodbc.h>
+
+#include "userDefine.h"
+
+enum STATUS
+{
+	PENDING,
+	IN_PROGRESS,
+	COMPLETED,
+	UNKNOWN
+};
+
+struct TASK
+{
+	int id = 0;
+	int assigneeId = 0;
+	std::string title = "";
+	std::string description = "";
+	STATUS status = STATUS::UNKNOWN;
+	nanodbc::date dateOfCreation = { 0, 0, 0 };
+	int creatorId = 0;
+	nanodbc::date dateOfLastChange = { 0, 0, 0 };
+	int lastChangerId = 0;
+	bool isDeleted = false;
+};
